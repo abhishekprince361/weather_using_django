21c98f17a1bba0b886be580df0cdd99fb8545348
Abhishek Kumar <abhishekprince361@gmail.com>
2024-03-31 21:51:28 +0530

    firstCommit

---
+++ README.md
@@ -0,0 +1,1 @@
+# weather_using_django
---
+++ weather_app/views.py
@@ -0,0 +1,40 @@
+import datetime
+import requests
+from django.shortcuts import render
+
+
+def home(request):
+    if 'city' in request.POST:
+        city = request.POST['city']
+    else:
+        return render(request, "search.html")
+        # city = "New Delhi"
+
+    api_key = '40228924c4a3846ff0dd11a7ba543e17'
+    url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}"
+    PARAMS = {"units": "metric"}
+
+    try:
+        data = requests.get(url, PARAMS).json()
+
+        photos = data['weather'][0]['main']
+        temp = round(data['main']['temp'])
+        humidity = data['main']['humidity']
+        wind_speed = data['wind']['speed']
+        day = datetime.date.today()
+        temp_dict = {'humidity': humidity,
+                     'speed': wind_speed,
+                     'temp': temp,
+                     'day': day,
+                     'city': city,
+                     'photos': photos,
+                     }
+        return render(request, "index.html", temp_dict)
+
+    except:
+        mydict = {
+            'city': city,
+            'day': datetime.date.today(),
+            'boolean_temp': True,
+        }
+        return render(request, "search.html", mydict)
